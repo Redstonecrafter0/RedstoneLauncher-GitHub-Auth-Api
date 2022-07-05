2c2edf8165d9d9939a1b9798888ee7f54f5d3987
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2022-07-05 10:30:42 +0200

    update stuff

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/api')
-def hello_world():
-    return 'Hello World!'
+@app.route('/api/auth')
+def apiAuth():
+    code = request.args.get('code')
+    if code is None:
+        return 'missing parameter code', 400
+    else:
+        response = requests.post('https://github.com/login/oauth/access_token', json={
+            'client_id': os.environ.get('REDSTONELAUNCHER_CLIENTID'),
+            'client_secret': os.environ.get('REDSTONELAUNCHER_CLIENT_SECRET'),
+            'code': code
+        }, headers={
+            'Accept': 'application/json'
+        })
+        return response.text, response.status_code
